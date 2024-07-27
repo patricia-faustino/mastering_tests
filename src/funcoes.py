72739da0e3ca13c76fe9de8945e09031d0355259
Patricia Faustino <patriciafaustinobr@gmail.com>
2024-07-27 19:13:32 -0300

    feat: create functions to test

--- src/funcoes.py
+++ src/funcoes.py
@@ -1,3 +1,5 @@
+import time
+
 def email_is_valid(email):
     return "@" in email and "." in email
 
@@ -8,3 +10,21 @@ def divide(a, b):
 
 def double_sum(numbers):
     return sum( x* 2 for x in numbers)
+
+def sum_numbers(a, b):
+    return a + b
+
+def fatorial(n):
+    if not isinstance(n, int) or n < 0:
+        raise TypeError("O argumento deve ser um número inteiro e positivo")
+    if n == 0:
+        return 1
+    else:
+        return n * fatorial(n-1)
+    
+def slow_sum(a,b):
+    time.sleep(2)
+    return a + b
+
+def multiply(a, b):
+    return a * b
